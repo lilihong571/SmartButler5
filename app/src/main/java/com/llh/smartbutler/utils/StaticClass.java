2b942a9fb934e6f242a0474ed1a65495ea8f67dd
lilihong571 <1132929091@qq.com>
2019-07-19 10:24:09 +0800

    Fourth commit.

---
+++ app/src/main/java/com/llh/smartbutler/utils/StaticClass.java
@@ -0,0 +1,18 @@
+package com.llh.smartbutler.utils;
+
+/**
+ * 项目名:    SmartButler5
+ * 包名:      com.llh.smartbutler.utils
+ * 文件名:    StaticClass
+ * 创建者:    LLH
+ * 创建时间:  2019/6/27 9:12
+ * 描述:      TODO
+ */
+public class StaticClass {
+    public static final int HANDLER_MASSAGE = 1001;
+
+    public static final String HANDLER_KEY = "isFirst";
+
+    public static final String APPLICATION_ID = "acb248ef04966572158796160f155c23";
+
+}
